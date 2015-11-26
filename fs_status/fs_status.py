4bdd40997cc1bc5765694bb6c93880477c982c57
yuniersoad <yunier.soad@gmail.com>
2015-11-26 15:07:33 -0500

    More generic fuction to get info from df command, included inode info

--- fs_status/fs_status.py
+++ fs_status/fs_status.py
@@ -2,29 +2,30 @@ import os
 import subprocess 
 
 FILE_SYSTEM = '/dev/disk0s2s1'
+
 SIZE_COLUMN = 'Size'
 USED_COLUMN = 'Used'
 USED_PERCENT_COLUMN = 'Capacity'
 
-def table_info_from_command3(command):
+INODE_USED_COLUMN = 'iused'
+INODE_FREE_COLUMN = 'ifree'
+INODE_USED_PERCENT_COLUMN = '%iused'
+
+def table_info_from_command(command, row_start, columns):
 	output = subprocess.Popen(command, stdout=subprocess.PIPE).communicate()[0]
 
 	header_line = output.split('\n')[0]
 
 	headers = header_line.split()
-	size_index = headers.index(SIZE_COLUMN)
-	used_index = headers.index(USED_COLUMN) 
-	used_percent_index = headers.index(USED_PERCENT_COLUMN)
+        indexes = [headers.index(col) for col in columns]
 
 
-	fs_line = next((line.strip() for line in  output.split('\n') if line.strip().startswith(FILE_SYSTEM)), "")
+	fs_line = next((line.strip() for line in  output.split('\n') if line.strip().startswith(row_start)), "")
 
 	values = fs_line.split()
-	size = values[size_index]
-	used = values[used_index]
-	used_percent = values[used_percent_index]
 
-	return [size, used, used_percent]
+	return [values[i] for i in indexes]
 
-print table_info_from_command3(["df", "-h"])
+print table_info_from_command(["df", "-h"], FILE_SYSTEM, [SIZE_COLUMN, USED_COLUMN, USED_PERCENT_COLUMN])
+print table_info_from_command(["df", "-hi"], FILE_SYSTEM, [INODE_USED_COLUMN, INODE_FREE_COLUMN, INODE_USED_PERCENT_COLUMN])
 
